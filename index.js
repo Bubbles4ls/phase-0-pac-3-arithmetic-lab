936b02cf27ff37af92d721ae984bca191e0a9a78
Surayah Holt <+>
2023-02-27 23:30:33 -0500

    Complete Lab

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+n = 295
+function increment(n) {
+
+    return ( n + 1);
+}
+   
+n = 112
+function decrement(n) {
+ return ( n - 1);
+
+}
+
+function makeInt(string) {
+
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+
+    return parseFloat(string);
+}
+
+
+
+
+
+
+
+
